63fcf9406d43738ff5ab9e87bd6494e50192c4ab
Shishand <shivamshandilya50@gmail.com>
2026-03-28 12:49:08 +0530

    Delete claude_code_projects/kong-ecommerce-demo directory

--- claude_code_projects/kong-ecommerce-demo/scripts/init-db.sql
+++
@@ -1,6 +0,0 @@
--- Initialize databases for each microservice
--- This script runs once when the postgres container is first created.
-
-CREATE DATABASE products_db;
-CREATE DATABASE orders_db;
-CREATE DATABASE users_db;
